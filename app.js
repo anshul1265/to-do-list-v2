72a026dcfe4dcd2e315d55d906555a25fe5cadc3
Anshul <Anshul@anshul.com>
2022-09-26 10:12:26 +0530

    final commit

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ mongoose.connect(process.env.MONGO_DB_URL).then((c) => {
 	console.log(`mongo connected : ${c.connection.host}`);
 }).catch(e => {
 	console.log(e);
-})
+});
 
 // making the schema for the items
 const itemsSchema = {
